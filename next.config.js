99a9f6a303e4482b9b99da66d15a8a364d61e7ca
Steven <53283472+binolt@users.noreply.github.com>
2021-11-12 20:11:33 -0500

    update next.config.js with css webpack support

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const {MONGODB_URI, TOKEN_SECRET, GITHUB_CALLBACK_URL, GITHUB_CLIENT_SECRET, GITHUB_CLIENT_ID} = process.env;
 
+if (typeof require !== "undefined") {
+  require.extensions[".less"] = () => {};
+  require.extensions[".css"] = (file) => {};
+}
+
 module.exports = {
     env: {
       "MONGODB_URI": MONGODB_URI,
