8d5d7ab9968a3b2e5e41a3d18041b52da42e31ca
Steven <53283472+binolt@users.noreply.github.com>
2021-11-13 22:49:33 -0500

    establish new product in DB / err handling
    add new product to DB and add extensive error handling for all functions surrounding the product create route.

--- pages/api/product/create.js
+++ pages/api/product/create.js
@@ -1,48 +1,51 @@
 import nextConnect from "next-connect";
 import { uploadFile, formatBufferTo64 } from "../../../utils/multer";
-import {newStripeProduct, uploadImage, newStripePrice} from "../../../lib/product";
+import {newStripeProduct, uploadImage, newStripePrice, createProduct} from "../../../lib/product";
 
 const handler = 
     nextConnect()
     .use(uploadFile)
     .post(async(req, res) => {
+
+        //ensure file upload
         if(!req.file) {
             res.json({msg: "You must upload a file !", error: true})
             return;
         }
 
-        try {
-            //upload image
-            const file64 = formatBufferTo64(req.file);
-            const {image, uploadError} = await uploadImage(file64);
-            if(uploadError) {
-                res.status(500).json(uploadError);
-                return;
-            }
+        // //upload image
+        // const file64 = formatBufferTo64(req.file);
+        // const {image, uploadError} = await uploadImage(file64);
+        // if(uploadError) {
+        //     res.status(500).json(uploadError);
+        //     return;
+        // }
 
-            //create new stripe product
-            const {id, productError} = await newStripeProduct(req.body.title, image);
-            if(productError) {
-                res.status(500).json(productError);
-                return;
-            }
+        // //create new stripe product
+        // const {id, productError} = await newStripeProduct(req.body.title, image);
+        // if(productError) {
+        //     res.status(500).json(productError);
+        //     return;
+        // }
 
-            //create a new stripe price 
-            const {priceId, priceError} = await newStripePrice(id, req.body.price);
-            if(priceError) {
-                res.status(500).json(productError);
-                return;
-            }
+        // //create a new stripe price 
+        // const {priceId, priceError} = await newStripePrice(id, req.body.price);
+        // if(priceError) {
+        //     res.status(500).json(productError);
+        //     return;
+        // }
 
+        const priceId = '1234kdfgjalsfsd'
 
-            //create new product
-
-            res.json({msg: "Image successfully uploaded !", error: false, productId: id, priceId})
-        } catch (err) {
-            if(err) throw new Error(err);
-            console.log(err)
-            res.status(500).json({msg: "something went wrong :/", error: true})
+        //register new product in database
+        const payload = {...req.body};
+        const {data, error} = await createProduct(payload);
+        if(error) {
+            res.status(500).json(error);
+            return;
         }
+
+        res.status(201).json(data)
 });
 
 export default handler;
