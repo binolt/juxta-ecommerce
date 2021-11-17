240b37a3c54be5cbe3946a49902b9f8d131977e1
Steven <murkappco@gmail.com>
2021-11-17 11:17:02 -0500

    add server side pre-render for individual product route
    add ssr and fetch product using query in getServerSideProps

--- pages/product/[title].js
+++ pages/product/[title].js
@@ -5,7 +5,8 @@ const baseUrl = (process.env.NODE_ENV === "development") ? DEV_URL : PROD_URL;
 export default function Product(props) {
     return (
         <div>
-            {props.msg}
+            <p>PID: {props.pid}</p>
+            {props.product && props.product.title}
         </div>
     );
 };
@@ -13,14 +14,24 @@ export default function Product(props) {
 
 export const getServerSideProps = async (ctx) => {
     // const {pid} = ctx.query;
+    const {pid} = ctx.query;
+    console.log(pid)
 
-    // // fetch product based off pid
-    // const res = await fetch(`${baseUrl}/api/product/${pid}`);
-    // const product = await res.json();
+    // fetch product based off pid
+    const res = await fetch(`${baseUrl}/api/product/${pid}`);
+    const product = await res.json();
+
+    if(product) {
+        return {
+            props: {
+                product, pid
+            }
+        }
+    }
 
     return {
         props:{
-            msg: "hello"
+            pid
         }
     }
 }
