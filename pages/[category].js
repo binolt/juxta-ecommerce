969b963924fe4ecd93ed8b708aaa6dad7eb3469e
Steven <53283472+binolt@users.noreply.github.com>
2021-11-16 22:55:36 -0500

    remove static generation for product listing page

--- pages/[category].js
+++ pages/[category].js
@@ -8,6 +8,7 @@ const baseUrl = (process.env.NODE_ENV === "development") ? DEV_URL : PROD_URL;
 
 export default function Category(props) {
     const router = useRouter();
+    const {category} = router.query;
 
     const handleClick = (target) => {
         const title = target.title.toLowerCase().replace("/", " ");
@@ -17,38 +18,39 @@ export default function Category(props) {
     return (
         <div>
             <div style={{display: 'flex'}}>
-            {props.products.map((product => {
+                <p>{category}</p>
+            {/* {props.products.map((product => {
                 return (
                     <div onClick={() => handleClick(product)} key={product._id} style={{width: 250, backgroundColor: 'orange'}}>
                         <img src={product.image} style={{width: 250}}/>
                         <p>{product.title}</p>
                     </div>
                 )
-            }))}
+            }))} */}
             </div>
         </div>
     );
 }
 
-export const getStaticProps = async (ctx) => {
-    const {category} = ctx.params;
+// export const getStaticProps = async (ctx) => {
+//     const {category} = ctx.params;
 
-    //fetch products based off category
-    const res = await fetch(`${baseUrl}/api/shop/${category}`);
-    const products = await res.json();
+//     //fetch products based off category
+//     const res = await fetch(`${baseUrl}/api/shop/${category}`);
+//     const products = await res.json();
 
-    return {
-        props: { products }
-    }
-}
+//     return {
+//         props: { products }
+//     }
+// }
 
-export const getStaticPaths = async () => {
-    const paths = DEFAULT_CATEGORIES.map((category) => ({
-        params: { category },
-    }))
+// export const getStaticPaths = async () => {
+//     const paths = DEFAULT_CATEGORIES.map((category) => ({
+//         params: { category },
+//     }))
 
-    return {
-        paths,
-        fallback:false
-    }
-}
+//     return {
+//         paths,
+//         fallback:false
+//     }
+// }
