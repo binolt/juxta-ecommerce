18b6ed4b56c36c696a0234000a3f8c8e45835cde
Steven <53283472+binolt@users.noreply.github.com>
2021-11-25 19:01:33 -0500

    clean up checkout route

--- pages/checkout.js
+++ pages/checkout.js
@@ -5,6 +5,7 @@ import { parseCookies, setCookie } from "nookies";
 import { useCart } from "../context/CartContext";
 import {loadStripe} from '@stripe/stripe-js';
 import { Elements, CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { fetchPaymentIntent, appendInvoiceItems, finalizeStripeInvoice, createStripeCustomer, createStripeInvoice, calculateShipping } from "../lib/stripe";
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
@@ -235,90 +236,6 @@ function CheckoutPayment() {
     )
 }
 
-const createStripeCustomer = async() => {
-    //fetch billing_details from local storage
-    const local_billing_details = await checkoutStorage.getItem("billing_details");
-    const { first_name, last_name, email_address, address_line_1, city, state, country, zipcode } = local_billing_details;
-
-    //format data for stripe
-    const payload = {
-        name: first_name + ' ' + last_name,
-        email: email_address,
-        address: {
-            line1: address_line_1,
-            city: city,
-            state: state,
-            country: country,
-            postal_code: zipcode
-        }
-    }
-
-    //create new customer
-    const { id, error } = await CheckoutService.createCustomer(payload);
-
-    //error handling
-    if(error) throw new Error(error.msg, error.type);
-
-    return id;
-}
-
-const calculateShipping = async(customer_id) => {
-    //fetch delivery_preference from local storage
-    const local_delivery_preference = await checkoutStorage.getItem("delivery_preference");
-
-    //append shipping item to customer if applicable
-    if(local_delivery_preference.shipping) {
-        //format data for stripe
-        const payload = {customer: customer_id, price: 'price_1JnsbPCABtvExHfR594lIIEZ', quantity: 1};
-        //create invoice item
-        const { error } = await CheckoutService.createInvoiceItem(payload);
-        //handle errors
-        if(error) throw new Error(error.msg, error.type);
-    }
-}
-
-const createStripeInvoice = async(customer_id) => {
-    //create invoice on customer
-    const { invoice_id, error } = await CheckoutService.createInvoice(customer_id);
-    //handle errors
-    if(error) throw new Error(error.msg, error.type)
-    return invoice_id;
-}
-
-const finalizeStripeInvoice = async(invoice_id) => {
-    //finalize invoice
-    const {finalized_invoice, error} = await CheckoutService.finalizeInvoice(invoice_id);
-    //handle errors
-    if(error) throw new Error(error.msg, error.type)
-    return finalized_invoice;
-}
-
-export const appendInvoiceItems = async(cart, customerId) => {
-    //loops through the cart and creates invoice items for each product on the newly created user
-    const fn = function createItem(product) {
-        return new Promise(async(resolve) => {
-            const {priceId, quantity} = product;
-            const payload = {customer: customerId, price: priceId, quantity: quantity};
-            await CheckoutService.createInvoiceItem(payload);
-            resolve();
-        })
-    }
-
-    const actions = cart.map(fn);
-    await Promise.all(actions);
-
-    return;
-}
-
-const fetchPaymentIntent = async(payment_intent_id) => {
-    //fetch payment intent
-    const { error, payment_intent } = await CheckoutService.retrievePaymentIntent(payment_intent_id);
-    //handle errors
-    if(error) throw new Error(error.msg, error.type)
-    return payment_intent;
-}
-
-
 const confirmCardPayment = async(payment_intent, elements, stripe) => {
     const {error, paymentIntent: {status}} = await stripe.confirmCardPayment(payment_intent.client_secret, {
         payment_method: {
@@ -328,9 +245,4 @@ const confirmCardPayment = async(payment_intent, elements, stripe) => {
     //handle errors
     if(error) throw new Error(error.msg, error.type)
     return status;
-}
-
-function Error(message, type) {
-    this.message = message;
-    this.type = type;
 }
