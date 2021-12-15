ad3ae8945624407d0226f8856c5998131763e583
Steven <53283472+binolt@users.noreply.github.com>
2021-12-15 17:16:41 -0500

    add scss modules

--- components/navbar.js
+++ components/navbar.js
@@ -1,48 +1,9 @@
-import { useAuth } from "../context/AuthContext";
-import AuthService from "../services/AuthService";
-import { useRouter } from "next/router";
-import Drawer from '@mui/material/Drawer';
-import { useCart } from "../context/CartContext";
-import Cart from "./cart";
+import styles from "./navbar.module.scss";
 
 export default function Navbar() {
-    const {setUser, currentUser} = useAuth();
-    const {cartOpen, setCartOpen} = useCart();
-    const router = useRouter();
-
-    const handleLogout = async() => {
-        const res = await AuthService.logout();
-        setUser(res.user);
-    }
-
-    const handleRoute = (target) => {
-        router.push(`/${target}`);
-    }
-
     return (
-        <nav className="wrapper">
-            <div className="content">
-            <button onClick={() => handleRoute('')}>Home</button>
-            <button onClick={() => handleRoute('mens')}>Shop</button>
-            {Object.keys(currentUser).length > 0 && <button onClick={() => handleRoute('dashboard')}>Dashboard</button>}
-            <span className="flexend">
-                <button onClick={() => setCartOpen(true)}>Cart</button>
-                {Object.keys(currentUser).length > 0 ? 
-                    <button onClick={handleLogout}>Logout</button> : (
-                    <>
-                    <button onClick={() => handleRoute('login')}>Login</button>
-                    <button onClick={() => handleRoute('register')}>Register</button>
-                    </>
-                )}
-            </span>
-            </div>
-            <Drawer
-                anchor='right'
-                open={cartOpen}
-                onClose={() => setCartOpen(false)}
-            >
-                <Cart/>
-            </Drawer>
+        <nav className={styles.nav}>
+            <p>navbar</p>
         </nav>
-    );
+    )
 }
